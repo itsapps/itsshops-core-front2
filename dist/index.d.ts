88a0724986a5c952582c2bf920f63866ca6a7709
kampfgnu <dev@itsapps.at>
2026-01-18 10:15:29 +0100

    remove dist

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-declare const shopCoreFrontendPlugin: (eleventyConfig: any, options?: {}) => void;
-declare const hello: (text: string) => Response;
-
-export { hello, shopCoreFrontendPlugin };
--- dist/preview2.d.ts
+++
@@ -1,3 +0,0 @@
-declare const preview2: (text: string) => Promise<Response>;
-
-export { preview2 };
